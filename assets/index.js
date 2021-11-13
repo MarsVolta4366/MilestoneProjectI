95268e64a24012c073542320ad6bb8f1c6bef488
unknown <derek.slauson5@gmail.com>
2021-11-13 10:19:39 -0600

    Reformatted JS to make it more readable

--- assets/index.js
+++ assets/index.js
@@ -1,16 +1,46 @@
-let boxes = document.querySelectorAll(".box")
 let isPlayerXsTurn = true
-let playerSpan = document.getElementById("playerSpan")
 let winnerDisplay = document.getElementById("winnerDisplay")
 let turn = 0
 let isGameOver = false
-let reset = document.getElementById("reset")
-let resetScores = document.getElementById("resetScores")
 let applause = document.getElementById("applause")
 let crowdCry = document.getElementById("crowdCry")
 let clickSound = document.getElementById("click")
+let boxes = document.querySelectorAll(".box")
+let playerSpan = document.getElementById("playerSpan")
+let turnSoundOff = document.getElementById("turnSoundOff")
+let turnSoundOn = document.getElementById("turnSoundOn")
+let reset = document.getElementById("reset")
+let resetScores = document.getElementById("resetScores")
 
+// Display score of each player stored in local storage on page load
 displayScores()
+
+// Sound button logic
+function muteSound() {
+    applause.muted = true 
+    crowdCry.muted = true
+    clickSound.muted = true
+    localStorage.isMuted = true
+    turnSoundOff.classList.add("hide")
+    turnSoundOn.classList.remove("hide")
+}
+
+if(localStorage.isMuted) {
+    muteSound()
+}
+
+turnSoundOff.addEventListener("click", () => {
+    muteSound()
+})
+
+turnSoundOn.addEventListener("click", () => {
+    localStorage.removeItem("isMuted")
+    turnSoundOn.classList.add("hide")
+    turnSoundOff.classList.remove("hide")
+    applause.muted = false 
+    crowdCry.muted = false
+    clickSound.muted = false
+})
 
 // Reset button logic
 reset.addEventListener("click", () => {
@@ -45,44 +75,14 @@ if(amountOfGames%2 !== 0) {
     playerSpan.textContent = "O"
 }
 
-// Display current scores
-function displayScores() {
-    let xScore = document.getElementById("xScore")
-    let oScore = document.getElementById("oScore")
-    if(localStorage.xScore) {
-        xScore.textContent = localStorage.xScore
-    }
-    if(localStorage.oScore) {
-        oScore.textContent = localStorage.oScore
-    }
-}
-
-// Update the winners score in localStorage
-function updateScore(winner) {
-    if(winner === "X") {
-        if(localStorage.xScore) {
-            localStorage.xScore = Number(localStorage.xScore) + 1
-        } else {
-            localStorage.xScore = 1
+// When empty box is clicked call boxClicked
+boxes.forEach(box => {
+    box.addEventListener("click", () => {
+        if(box.textContent === "" && !isGameOver) {
+            boxClicked(box)
         }
-    } else if(winner === "O") {
-        if(localStorage.oScore) {
-            localStorage.oScore = Number(localStorage.oScore) + 1
-        } else {
-            localStorage.oScore = 1
-        }
-    }
-    displayScores()
-}
-
-// Display winner and end game
-function displayWinner(index) {
-    winnerDisplay.textContent = `${boxes[index].textContent} wins!`
-    applause.volume = .3
-    applause.play()
-    isGameOver = true
-    updateScore(boxes[index].textContent)
-}
+    })
+})
 
 // Fill box with approriate text based on player, switch to other players turn, call checkForWinner()
 function boxClicked(box) {
@@ -104,85 +104,85 @@ function boxClicked(box) {
     checkForWinner()
 }
 
+// Check for winner or tie and display if found
+function checkForWinner() {
+    if(boxes[0].textContent === boxes[1].textContent && boxes[0].textContent === boxes[2].textContent && boxes[0].textContent != "") {
+        displayWinner(0)
+        highlightWinningBoxes(0, 1, 2)
+    } else if(boxes[3].textContent === boxes[4].textContent && boxes[3].textContent === boxes[5].textContent && boxes[3].textContent != "") {
+        displayWinner(3)
+        highlightWinningBoxes(3, 4, 5)
+    } else if(boxes[6].textContent === boxes[7].textContent && boxes[6].textContent === boxes[8].textContent && boxes[6].textContent != "") {
+        displayWinner(6)
+        highlightWinningBoxes(6, 7, 8)
+    } else if(boxes[0].textContent === boxes[3].textContent && boxes[0].textContent === boxes[6].textContent && boxes[0].textContent != "") {
+        displayWinner(0)
+        highlightWinningBoxes(0, 3, 6)
+    } else if(boxes[1].textContent === boxes[4].textContent && boxes[1].textContent === boxes[7].textContent && boxes[1].textContent != "") {
+        displayWinner(1)
+        highlightWinningBoxes(1, 4, 7)
+    } else if(boxes[2].textContent === boxes[5].textContent && boxes[2].textContent === boxes[8].textContent && boxes[2].textContent != "") {
+        displayWinner(2)
+        highlightWinningBoxes(2, 5, 8)
+    } else if(boxes[0].textContent === boxes[4].textContent && boxes[0].textContent === boxes[8].textContent && boxes[0].textContent != "") {
+        displayWinner(0)
+        highlightWinningBoxes(0, 4, 8)
+    } else if(boxes[2].textContent === boxes[4].textContent && boxes[2].textContent === boxes[6].textContent && boxes[2].textContent != "") {
+        displayWinner(2)
+        highlightWinningBoxes(2, 4, 6)
+    } else if(turn === 9) {
+        winnerDisplay.textContent = "It's a tie!"
+        crowdCry.volume = .5
+        crowdCry.play()
+        isGameOver = true
+    }
+}
+
+// Display winner and end game
+function displayWinner(index) {
+    winnerDisplay.textContent = `${boxes[index].textContent} wins!`
+    applause.volume = .5
+    applause.play()
+    isGameOver = true
+    updateScore(boxes[index].textContent)
+}
+
 // Highlight winning boxes green and flash three times
 function highlightWinningBoxes(box1, box2, box3) {
-    boxes[box1].style.background = "#148f34"
+    boxes[box1].style.background = "#278841"
     boxes[box1].classList.add("winner")
-    boxes[box2].style.background = "#148f34"
+    boxes[box2].style.background = "#278841"
     boxes[box2].classList.add("winner")
-    boxes[box3].style.background = "#148f34"
+    boxes[box3].style.background = "#278841"
     boxes[box3].classList.add("winner")
 }
 
-// Check for winner or tie and display if found
-function checkForWinner() {
-        if(boxes[0].textContent === boxes[1].textContent && boxes[0].textContent === boxes[2].textContent && boxes[0].textContent != "") {
-            displayWinner(0)
-            highlightWinningBoxes(0, 1, 2)
-        } else if(boxes[3].textContent === boxes[4].textContent && boxes[3].textContent === boxes[5].textContent && boxes[3].textContent != "") {
-            displayWinner(3)
-            highlightWinningBoxes(3, 4, 5)
-        } else if(boxes[6].textContent === boxes[7].textContent && boxes[6].textContent === boxes[8].textContent && boxes[6].textContent != "") {
-            displayWinner(6)
-            highlightWinningBoxes(6, 7, 8)
-        } else if(boxes[0].textContent === boxes[3].textContent && boxes[0].textContent === boxes[6].textContent && boxes[0].textContent != "") {
-            displayWinner(0)
-            highlightWinningBoxes(0, 3, 6)
-        } else if(boxes[1].textContent === boxes[4].textContent && boxes[1].textContent === boxes[7].textContent && boxes[1].textContent != "") {
-            displayWinner(1)
-            highlightWinningBoxes(1, 4, 7)
-        } else if(boxes[2].textContent === boxes[5].textContent && boxes[2].textContent === boxes[8].textContent && boxes[2].textContent != "") {
-            displayWinner(2)
-            highlightWinningBoxes(2, 5, 8)
-        } else if(boxes[0].textContent === boxes[4].textContent && boxes[0].textContent === boxes[8].textContent && boxes[0].textContent != "") {
-            displayWinner(0)
-            highlightWinningBoxes(0, 4, 8)
-        } else if(boxes[2].textContent === boxes[4].textContent && boxes[2].textContent === boxes[6].textContent && boxes[2].textContent != "") {
-            displayWinner(2)
-            highlightWinningBoxes(2, 4, 6)
-        } else if(turn === 9) {
-            winnerDisplay.textContent = "It's a tie!"
-            crowdCry.volume = .4
-            crowdCry.play()
-            isGameOver = true
+// Update the winners score in localStorage
+function updateScore(winner) {
+    if(winner === "X") {
+        if(localStorage.xScore) {
+            localStorage.xScore = Number(localStorage.xScore) + 1
+        } else {
+            localStorage.xScore = 1
         }
-}
-
-boxes.forEach(box => {
-    box.addEventListener("click", () => {
-        // When empty box is clicked call boxClicked
-        if(box.textContent === "" && !isGameOver) {
-            boxClicked(box)
+    } else if(winner === "O") {
+        if(localStorage.oScore) {
+            localStorage.oScore = Number(localStorage.oScore) + 1
+        } else {
+            localStorage.oScore = 1
         }
-    })
-})
-
-// Sound button
-let turnSoundOff = document.getElementById("turnSoundOff")
-let turnSoundOn = document.getElementById("turnSoundOn")
-
-function muteSound() {
-    applause.muted = true 
-    crowdCry.muted = true
-    clickSound.muted = true
-    localStorage.isMuted = true
-    turnSoundOff.classList.add("hide")
-    turnSoundOn.classList.remove("hide")
+    }
+    displayScores()
 }
 
-if(localStorage.isMuted) {
-    muteSound()
+// Display current scores
+function displayScores() {
+    let xScore = document.getElementById("xScore")
+    let oScore = document.getElementById("oScore")
+    if(localStorage.xScore) {
+        xScore.textContent = localStorage.xScore
+    }
+    if(localStorage.oScore) {
+        oScore.textContent = localStorage.oScore
+    }
 }
-
-turnSoundOff.addEventListener("click", () => {
-    muteSound()
-})
-
-turnSoundOn.addEventListener("click", () => {
-    localStorage.removeItem("isMuted")
-    turnSoundOn.classList.add("hide")
-    turnSoundOff.classList.remove("hide")
-    applause.muted = false 
-    crowdCry.muted = false
-    clickSound.muted = false
-})
